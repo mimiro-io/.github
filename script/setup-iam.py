be238f8c1a54abe06663436a3663af6f69c68649
akshaykrjain <akshay.kumar.jain@mimiro.no>
2021-11-12 10:51:47 +0100

    updated script

--- script/setup-iam.py
+++ script/setup-iam.py
@@ -1,112 +1,82 @@
-import json, boto3
+import json
+import boto3
+import os
 from botocore.exceptions import ClientError
 
 #IRSA = IAM Role for Service Account
 def get_eks_oidc():
-    eks_client = boto3.client('eks')
-    response = eks_client.list_clusters(maxResults=1)
-    response = eks_client.describe_cluster(name=(response['clusters'])[0])
-    cluster_details=(response['cluster'])
-    oidc_issuer=((cluster_details["identity"])["oidc"])["issuer"]
-    print(oidc_issuer)
-    # #print(response.clusters)
-    # response = eks_client.describe_cluster(
-    #     name='mimiro-k8s'
-    # )   
-    # print(response['cluster'])
+    try:
+        response = eks_client.list_clusters(maxResults=1)
+        no_of_clusters = len(response['clusters'])
+        if no_of_clusters != 1:
+            print ('Script supports only if there is only one eks cluster. Expected : 1 Got:', no_of_clusters)
+            print ('Not able to create/update IRSA. Pls try again.')
+            exit(1)
+        response = eks_client.describe_cluster(name=(response['clusters'])[0])
+        cluster_details=(response['cluster'])
+        oidc_issuer=((cluster_details["identity"])["oidc"])["issuer"]
+        return oidc_issuer.replace("https://","") 
+    except ClientError as error:
+            return 'Unexpected error occurred while fetching cluster details', error
 
-    # for i in response['cluster']:
-    #     print(i.name)
-
-    # #print (response.keys)
-    # #return "OK"
-    #client = boto3.client('ecs')
-    #response = client.list_services(cluster="Fargate",maxResults=100)
-    #print(response)
-    #servicearns = (response['serviceArns'])
-    #print(servicearns)
-
-def create_role():
-    iam_client = boto3.client('iam')
-    role_name = "k8s-temp-tbd"
-    account_id = "313146451641"
-    trust_relationship_policy_another_aws_service = {
+def get_assume_role_policy():
+    oidc_issuer_url=get_eks_oidc()
+    trust_relationship_policy = {
         "Version": "2012-10-17",
         "Statement": [
             {
-                "Effect": "Allow",
-                "Principal": {
-                    "Service": "ec2.amazonaws.com"
-                },
-                "Action": "sts:AssumeRole"
+            "Effect": "Allow",
+            "Principal": {
+                "Federated": "arn:aws:iam::"+account_id+":oidc-provider/"+oidc_issuer_url
+            },
+            "Action": "sts:AssumeRoleWithWebIdentity",
+            "Condition": {
+                "StringEquals": {
+                ""+oidc_issuer_url+":sub": "system:serviceaccount:mimiro:mimiro-web-auth",
+                ""+oidc_issuer_url+":aud": "sts.amazonaws.com"
+                }
+            }
             }
         ]
     }
+    return json.dumps(trust_relationship_policy)
+role_name = "mimiro-k8s-"+os.environ.get('APP')+"-role" #TODO- Get prefix from eks cluster name
+print('IRSA:',role_name, ': Setting up IAM Roles & Policy for the service account')
+iam_client = boto3.client('iam')
+eks_client = boto3.client('eks')
+sts_client = boto3.client('sts')
+account_id = sts_client.get_caller_identity().get('Account')
+assume_role_policy=get_assume_role_policy()
 
-    #Following trust relationship policy can be used to provide access to assume this by third party using external id
+try:
+    response = iam_client.get_role(RoleName=role_name)
+    print ('IRSA:',role_name,': Role exists! updating existing role and policy')
     try:
-        create_role_res = iam_client.create_role(
+        response = iam_client.update_assume_role_policy(
             RoleName=role_name,
-            AssumeRolePolicyDocument=json.dumps(trust_relationship_policy_another_aws_service),
-            Description='This is a test role',
+            PolicyDocument=assume_role_policy
+        )
+    except ClientError as error:
+        print('IRSA:',role_name, ': Unexpected error occurred... Role could not be updated', error)
+        exit(1)
+    print('IRSA:',role_name, ': Role successfully updated')
+except ClientError as error:
+    if error.response['Error']['Code'] == 'NoSuchEntity':
+        print ('IRSA:',role_name,': Role does not exist.Creating new role and policy')
+        try:
+            create_role_res = iam_client.create_role(
+            RoleName=role_name,
+            AssumeRolePolicyDocument=get_assume_role_policy(),
+            Description=role_name,
             Tags=[
                 {
-                    'Key': 'Owner',
-                    'Value': 'ops'
+                    'Key': 'CreatedBy',
+                    'Value': 'ReusableGithubActions'
                 }
             ]
         )
-    except ClientError as error:
-        if error.response['Error']['Code'] == 'EntityAlreadyExists':
-            return 'Role already exists... hence exiting from here'
-        else:
-            return 'Unexpected error occurred... Role could not be created', error
+        except ClientError as error:
+            print('IRSA:',role_name, ': Unexpected error occurred... Role could not be created', error)
+            exit(1)
+        print('IRSA:',role_name, ': Role Successfully Created')
 
-    policy_json = {
-        "Version": "2012-10-17",
-        "Statement": [{
-            "Effect": "Allow",
-            "Action": [
-                "ec2:*"
-            ],
-            "Resource": "*"
-        }]
-    }
-
-    policy_name = role_name + '_policy'
-    policy_arn = ''
-
-    try:
-        policy_res = iam_client.create_policy(
-            PolicyName=policy_name,
-            PolicyDocument=json.dumps(policy_json)
-        )
-        policy_arn = policy_res['Policy']['Arn']
-    except ClientError as error:
-        if error.response['Error']['Code'] == 'EntityAlreadyExists':
-            print('Policy already exists... hence using the same policy')
-            policy_arn = 'arn:aws:iam::' + account_id + ':policy/' + policy_name
-        else:
-            print('Unexpected error occurred... hence cleaning up', error)
-            iam_client.delete_role(
-                RoleName= role_name
-            )
-            return 'Role could not be created...', error
-
-    try:
-        policy_attach_res = iam_client.attach_role_policy(
-            RoleName=role_name,
-            PolicyArn=policy_arn
-        )
-    except ClientError as error:
-        print('Unexpected error occurred... hence cleaning up')
-        iam_client.delete_role(
-            RoleName= role_name
-        )
-        return 'Role could not be created...', error
-
-    return 'Role {0} successfully got created'.format(role_name)
-
-response = get_eks_oidc()
-#response = create_role()
-#print (response)
